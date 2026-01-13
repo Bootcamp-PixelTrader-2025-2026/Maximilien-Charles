94bdd3563d7e713debdbc2745d76806c50158a20
max <maximilien.ilic@gmail.com>
2026-01-13 11:42:15 +0100

    ajout function tri

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ import parser from 'csv-parser';
 
 export const readCsvFile = () => {
 	const filePath = './stock_legacy_full.csv';
-	const fileExists = fs.readFileSync(filePath);
 
-	if (!fileExists) {
+	if (!fs.existsSync(filePath)) {
 		throw new Error(`Le fichier ${filePath} n'existe pas.`);
 	}
 
-	let results = [];
-	fs.createReadStream(filePath)
-		.pipe(parser())
-		.on('data', (row) => results.push(row))
-		.on('end', () => {
-			console.log(results);
-		});
+	return new Promise((resolve, reject) => {
+		const results = [];
+
+		fs.createReadStream(filePath)
+			.pipe(parser())
+			.on('data', (row) => results.push(tri(row)))
+			.on('end', () => resolve(results))
+			.on('error', reject);
+	});
 };
 
-readCsvFile();
+function tri(json) {
+	const keyString = Object.keys(json)[0];
+	const valueString = Object.values(json)[0];
+
+	const keys = keyString.split(';');
+	const values = valueString.split(';');
+
+	return Object.fromEntries(
+		keys.map((key, i) => [key, values[i] ?? null])
+	);
+}
+
+(async () => {
+	const data = await readCsvFile();
+	console.log(data[0].titre_jeu);
+})();
