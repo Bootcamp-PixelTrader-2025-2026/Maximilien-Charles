db7bd89b8d92fe20e2752c7d6515f11fa4fbd897
carls-eth <charles.bauchet@edu.devinci.fr>
2026-01-15 11:58:10 +0100

    add : MAJ API

---
+++ API/stock_values.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Content-Type: application/json');
+
+try {
+    $pdo = new PDO(
+        'mysql:host=localhost;port=8889;dbname=pixeltrader;charset=utf8',
+        'root',
+        'root',
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
+
+    $requete = $pdo->prepare ("SELECT * FROM stock_values");
+    
+    $requete->execute();
+
+    $retour = [
+        "success" => true,
+        "data" => $requete->fetchAll(PDO::FETCH_ASSOC)
+    ];
+
+} catch (PDOException $e) {
+    $retour = [
+        "success" => false,
+        "data" => $e->getMessage()
+    ];
+}
+
+echo json_encode($retour);
